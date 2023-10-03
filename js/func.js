c52853a68df6dbfd8337192f375422045ccdbcd2
giorg <giorg@DESKTOP-DCR8L1V>
2023-10-03 18:57:33 +0200

    Application Complete, limit Input Number to do

--- js/func.js
+++ js/func.js
@@ -8,7 +8,34 @@
 document.getElementById("launch").addEventListener("click",test);
 
 function test(){
+    
     let dicetype = document.getElementById("diceTypes").value;
     let launchnum = document.getElementById("launchNum").value;
-    document.getElementById("result").innerHTML = dicetype.concat(" ",launchnum);
+    let result = document.getElementById("result");
+    fetch("../php/work.php",{
+        method: "post",
+        headers: new Headers({
+            "Content-Type": "application/json"
+        }),
+        body: JSON.stringify({
+            dicetype : document.getElementById("diceTypes").value,
+            launchnum : document.getElementById("launchNum").value
+        })
+    }).then(function(response){
+        return response.json();
+    }).then(function(data){
+        while(result.childElementCount > 0){
+            result.removeChild(result.firstChild);
+        }
+        for (let item = 0; item < data.length; item++) {
+          let div = document.createElement("div");
+          div.innerHTML = data[item];
+          div.className = "dice";
+          //div.classList.add("dice");
+          result.appendChild(div);
+          //console.log(result);
+        }
+        //result.innerHTML = data;
+        console.log(data);
+    });
 }
--- php/work.php
+++ php/work.php
@@ -5,3 +5,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
 
+if(isset($_POST)){
+    $data = file_get_contents("php://input");
+    $dice = json_decode($data,true);
+    $num = preg_replace('/\D/', '', $dice["dicetype"]);
+    $repeat = $dice["launchnum"];
+    $result = array();
+    for ($index = 0; $index < $repeat; $index++) {
+        array_push($result, rand(1,$num));
+    }
+    echo json_encode($result);
+    //echo $dice["dicetype"]." ".$dice["launchnum"];
+}
